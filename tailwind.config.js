c3a2bc017228a3b61b4e1fd0f8da03e73a8a733e
daviszung <daviszung@gmail.com>
2023-07-28 19:44:39 -0400

    header progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-blue-violet':
+           'linear-gradient(141deg, #3A8EF6 0%, #6F3AFA 100%)'
       },
     },
   },
